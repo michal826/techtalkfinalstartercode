ad7c7b70c4517b2e070a9fe2a37078a543fda216
Michal Idzik <midzik826@gmail.com>
2017-07-07 19:35:08 +0000

    hello

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-// JavaScript File
-// JavaScript File
-$( document ).ready(function() {
-
-
+$(document).ready(function(){
+$("#alert").click(function(){
+  var answer = $("input").val();
+  if (answer === "King Henry VIII"){
+    alert("Great Job");
+  }else if (answer === "King Henry"){
+    alert("Close");
+  }else{
+    alert("Try Again");
+  }
+});
+$("#buttons").click(function(){
+  for(var i=0; i<5; i=i+1){
+  $("div").append('<img src="https://media1.giphy.com/media/xT8qBsOjMOcdeGJIU8/giphy.gif">') 
+}
+});
 });
